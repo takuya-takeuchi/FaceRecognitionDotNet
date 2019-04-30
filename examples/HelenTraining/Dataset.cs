8b5f41955142b282f6ab1ef9f0e45f16441308bb
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-05-01 05:13:38 +0900

    feat: add HelenTraining example

---
+++ examples/HelenTraining/Dataset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace HelenTraining
+{
+
+    [Serializable]
+    [XmlRoot(ElementName = "dataset")]
+    public class Dataset
+    {
+
+        [XmlElement("name")]
+        public string Name;
+
+        [XmlElement("comment")]
+        public string Comment;
+
+        [XmlArray("images")]
+        [XmlArrayItem("image")]
+        public Image[] Images;
+
+    }
+
+}
