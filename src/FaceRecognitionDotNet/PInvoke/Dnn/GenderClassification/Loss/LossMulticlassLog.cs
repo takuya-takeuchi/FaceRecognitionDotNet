124b8cb1be2a0bff3fe6b7f127a37eba263f8d1a
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-09-06 01:16:19 +0900

    feat: add method to classify human gender from face

---
+++ src/FaceRecognitionDotNet/PInvoke/Dnn/GenderClassification/Loss/LossMulticlassLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once CheckNamespace
+namespace DlibDotNet
+{
+
+    internal sealed class NativeMethods
+    {
+
+        #region Fields
+
+        public const string NativeLibrary = "DlibDotNetNativeDnnGenderClassification";
+
+        public const CallingConvention CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl;
+        
+        #endregion
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern IntPtr LossMulticlassLog_gender_train_type_create();
+
+    }
+
+}
