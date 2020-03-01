c4391aa80f693738ec4d8a77d4c2411a0f717187
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-03-01 14:25:05 +0900

    misc: add publish script

---
+++ nuget/Publish.bat
@@ -0,0 +1,5 @@
+dotnet nuget push FaceRecognitionDotNet.MKL.%1.nupkg -k %2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push FaceRecognitionDotNet.CUDA101.%1.nupkg -k %2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push FaceRecognitionDotNet.CUDA100.%1.nupkg -k %2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push FaceRecognitionDotNet.CUDA92.%1.nupkg -k %2 -s https://api.nuget.org/v3/index.json
+dotnet nuget push FaceRecognitionDotNet.%1.nupkg -k %2 -s https://api.nuget.org/v3/index.json
