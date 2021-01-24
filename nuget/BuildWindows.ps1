c415df1178a4740e8eb19316e3e53c24cb9d25ae
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-01-25 01:47:57 +0900

    feat: support cuda11

--- nuget/BuildWindows.ps1
+++ nuget/BuildWindows.ps1
@@ -38,6 +38,8 @@ $BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture =
 $BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture = 64; RID = "$OperatingSystem-x64";   CUDA = 100 }
 $BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture = 64; RID = "$OperatingSystem-x64";   CUDA = 101 }
 $BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture = 64; RID = "$OperatingSystem-x64";   CUDA = 102 }
+$BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture = 64; RID = "$OperatingSystem-x64";   CUDA = 110 }
+$BuildTargets += New-Object PSObject -Property @{Target = "cuda"; Architecture = 64; RID = "$OperatingSystem-x64";   CUDA = 111 }
 
 foreach ($BuildTarget in $BuildTargets)
 {
