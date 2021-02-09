9a3f39aa4df48576e5366ea105889caf5bb2552d
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-02-09 21:57:00 +0900

    Add new interface for Create (#148)
    * feat: implement new overload for Create

---
+++ src/FaceRecognitionDotNet/ModelParameter.cs
@@ -0,0 +1,52 @@
+﻿namespace FaceRecognitionDotNet
+{
+
+    /// <summary>
+    /// Describes the model binary datum. This class cannot be inherited.
+    /// </summary>
+    public sealed class ModelParameter
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the binary data of model for 68 points face landmarks.
+        /// </summary>
+        public byte[] PosePredictor68FaceLandmarksModel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the binary data of model for 5 points face landmarks.
+        /// </summary>
+        public byte[] PosePredictor5FaceLandmarksModel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the binary data of model for face encoding.
+        /// </summary>
+        public byte[] FaceRecognitionModel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the binary data of model for face detector by using CNN.
+        /// </summary>
+        public byte[] CnnFaceDetectorModel
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+    }
+
+}
