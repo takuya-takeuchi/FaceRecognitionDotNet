9a7651cdc5bcd2518ffdf744346bf512b694e9f9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-01-30 12:39:40 +0900

    fix

---
+++ src/FaceRecognitionDotNet/PInvoke/Dnn/EmotionClassification/Loss/LossMulticlassLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once CheckNamespace
+namespace DlibDotNet
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        #region Fields
+
+        public const string EmotionClassificationNativeLibrary = "DlibDotNetNativeDnnEmotionClassification";
+
+        public const CallingConvention EmotionClassificationCallingConvention = CallingConvention.Cdecl;
+
+        #endregion
+
+        [DllImport(EmotionClassificationNativeLibrary, CallingConvention = EmotionClassificationCallingConvention)]
+        public static extern IntPtr LossMulticlassLog_emotion_train_type_create();
+
+        [DllImport(EmotionClassificationNativeLibrary, CallingConvention = EmotionClassificationCallingConvention)]
+        public static extern void LossMulticlassLog_emotion_train_type_delete(IntPtr @base);
+
+        [DllImport(EmotionClassificationNativeLibrary, CallingConvention = EmotionClassificationCallingConvention)]
+        public static extern void LossMulticlassLog_emotion_train_type_eval(IntPtr @base);
+
+
+    }
+
+}
