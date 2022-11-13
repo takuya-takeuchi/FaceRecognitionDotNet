214c4fda18ec417dd43479a39691ef96dde2ad3c
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-11-13 13:53:50 +0900

    fix

--- src/FaceRecognitionDotNet/Extensions/AnnoySearch.cs
+++ src/FaceRecognitionDotNet/Extensions/AnnoySearch.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 
 using DlibDotNet;
 
@@ -8,7 +7,7 @@ namespace FaceRecognitionDotNet.Extensions
 {
 
     /// <summary>
-    /// The head pose estimator which was trained by 300W-LP dataset. This class cannot be inherited.
+    /// The face search class that provides Annoy (Approximate Nearest Neighbors Oh Yeah) kind of Approximate Nearest Neighbors. This class cannot be inherited.
     /// </summary>
     public sealed class AnnoySearch : Search
     {
@@ -22,12 +21,8 @@ namespace FaceRecognitionDotNet.Extensions
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="AnnoySearch"/> class with the model files to estimate head pose.
+        /// Initializes a new instance of the <see cref="AnnoySearch"/>.
         /// </summary>
-        /// <param name="rollModelFile">The model file path to estimate roll angle.</param>
-        /// <param name="pitchModelFile">The model file path to estimate pitch angle.</param>
-        /// <param name="yawModelFile">The model file path to estimate yaw angle.</param>
-        /// <exception cref="FileNotFoundException"><paramref name="rollModelFile"/>, <paramref name="pitchModelFile"/> or <paramref name="yawModelFile"/> does not exist.</exception>
         public AnnoySearch()
         {
             this._Index = NativeMethods.AnnoySearch_AnnoyIndex_new(128);
@@ -37,39 +32,72 @@ namespace FaceRecognitionDotNet.Extensions
 
         #region Methods
 
+        /// <summary>
+        /// Add an <see cref="FaceEncoding"/> to feature data set.
+        /// </summary>
+        /// <param name="item">The label to specify <param name="encoding">.</param></param>
+        /// <param name="encoding">A known face encodings to be added to feature data set.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="encoding"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="encoding"/> or this object is disposed.</exception>
         public override void Add(int item, FaceEncoding encoding)
         {
+            if (encoding == null) 
+                throw new ArgumentNullException(nameof(encoding));
+
+            encoding.ThrowIfDisposed();
+
             this.ThrowIfDisposed();
             NativeMethods.AnnoySearch_AnnoyIndex_add_item(this._Index, item, encoding.Encoding.ToArray());
         }
 
+        /// <summary>
+        /// Build feature data set to make ready for query.
+        /// </summary>
         public override void Build()
         {
             this.ThrowIfDisposed();
-            // ToDo: I'm not sure why 2 *...
+            // ToDo: I'm not sure why 2 *. It could be tree size.
             //       https://github.com/spotify/annoy/blob/master/examples/precision_test.cpp#L50
             NativeMethods.AnnoySearch_AnnoyIndex_build(this._Index, 2 * 128);
         }
 
+        /// <summary>
+        /// Searches for elements that are closed to given face encoding, and returns the top K occurrence within the entire feature data set.
+        /// </summary>
+        /// <param name="encoding">A face encodings to query in feature data set.</param>
+        /// <param name="topK">The number of most likely outcomes to query the label.</param>
+        /// <returns>A dictionary of label and distance.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="encoding"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="encoding"/> or this object is disposed.</exception>
         public override IDictionary<int, double> Query(FaceEncoding encoding, uint topK)
         {
+            if (encoding == null)
+                throw new ArgumentNullException(nameof(encoding));
+
+            encoding.ThrowIfDisposed();
+
             this.ThrowIfDisposed();
 
             // ToDo: What is n parameter... it could be related to threading
-            using (var toplist = new StdVector<int>())
+            using (var topList = new StdVector<int>())
             using (var distances = new StdVector<double>())
             {
                 NativeMethods.AnnoySearch_AnnoyIndex_get_nns_by_vector(this._Index,
                                                                        encoding.Encoding.ToArray(),
-                                                                       (ulong)topK,
+                                                                       topK,
                                                                        -1,
-                                                                       toplist.NativePtr,
+                                                                       topList.NativePtr,
                                                                        distances.NativePtr);
 
                 var dictionary = new Dictionary<int, double>();
-                var count = toplist.Count;
+
+                var topListArray = topList.ToArray();
+                var distancesArray = distances.ToArray();
+
+                var count = topListArray.Length;
                 for (var index = 0; index < count; index++)
-                    dictionary.Add(toplist[index], distances[index]);
+                    dictionary.Add(topListArray[index], distancesArray[index]);
+                    
                 return dictionary;
             }
         }
--- src/FaceRecognitionDotNet/Extensions/Search.cs
+++ src/FaceRecognitionDotNet/Extensions/Search.cs
@@ -4,17 +4,31 @@ namespace FaceRecognitionDotNet.Extensions
 {
 
     /// <summary>
-    /// An abstract base class that provides functionality to estimate human head pose from face landmark.
+    /// An abstract base class that provides functionality to query face encoding from face encoding set.
     /// </summary>
     public abstract class Search : DisposableObject
     {
 
         #region Methods
 
+        /// <summary>
+        /// Add an <see cref="FaceEncoding"/> to feature data set.
+        /// </summary>
+        /// <param name="item">The label to specify <param name="encoding">.</param></param>
+        /// <param name="encoding">A known face encodings to be added to feature data set.</param>
         public abstract void Add(int item, FaceEncoding encoding);
 
+        /// <summary>
+        /// Build feature data set to make ready for query.
+        /// </summary>
         public abstract void Build();
 
+        /// <summary>
+        /// Searches for elements that are closed to given face encoding, and returns the top K occurrence within the entire feature data set.
+        /// </summary>
+        /// <param name="encoding">A face encodings to query in feature data set.</param>
+        /// <param name="topK">The number of most likely outcomes to query the label.</param>
+        /// <returns>A dictionary of label and distance.</returns>
         public abstract IDictionary<int, double> Query(FaceEncoding encoding, uint topK);
 
         #endregion
